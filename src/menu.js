0808a549cabce6dbb446968c09f09965fc63c581
Coralie <207925650+Coralie86@users.noreply.github.com>
2025-09-05 16:10:28 +0200

    Creation of Restaurant page using Webpack

---
+++ src/menu.js
@@ -0,0 +1,21 @@
+function menu() {
+    const content = document.querySelector('#content');
+    
+    if (content.contains(document.querySelector('#container'))) {
+        content.removeChild(document.querySelector('#container'))
+    }
+    const container = document.createElement("div");
+    container.id = "container";
+
+    const title = document.createElement("div");
+    title.id = "title";
+    title.textContent = "The Hungry Stomach";
+
+    
+
+    container.appendChild(title);
+
+    content.appendChild(container);
+}
+
+export {menu}
